2da9d1053d4f0e067d4c38730611e2df38f258ff
tl228 <tommy6147@163.com>
2017-08-09 09:31:31 +0800

    app.js index.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
+import TestCom from './components/TestCom'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    console.log('msg')
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TestCom />
       </div>
     );
   }
--- src/components/TestCom/index.js
+++ src/components/TestCom/index.js
@@ -1,2 +1,20 @@
 import React, { Component } from 'react';
 
+class TestCom extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 'Y',
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+export default TestCom
